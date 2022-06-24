b463663a9173eeb6f1d2967f87ca530cbdb9013c
leandro lima <leandro.desouzalima1@gmail.com>
2022-06-24 14:33:49 -0300

    new + create

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,5 +4,16 @@ class TasksController < ApplicationController
   end
 
   def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    task = Task.new(params.require(:task).permit(:title, :details))
+    task.save
+    redirect_to root_path
   end
 end
